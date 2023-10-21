11654aaf85210d214b8c420ed7dab6aacf1fbea2
Iyad Suleiman <jiedz@protonmail.com>
2023-10-21 22:51:19 +0200

    Finished Writing Basic Task Class

--- taskplanner/tasks.py
+++ taskplanner/tasks.py
@@ -2,11 +2,15 @@
 This module describes a task and the relation between parent and children tasks.
 '''
 
-#%% Imports
+# %% Imports
 from anytree import Node
-from datetime import datetime
-#%% Task
+from inspect import currentframe, getargvalues
+from datetime import datetime, date
+
+# %% Task
 PRIORITY_LEVELS = ["low", "medium", "high"]
+
+
 class Task(Node):
     '''
     This class defines a task.
@@ -22,13 +26,15 @@ class Task(Node):
 
     A task is also represented as a node of a tree. Therefore, every task except top-level tasks, has a parent.
     '''
+
     def __init__(self,
-                 parent:Node=None,
-                 name:str="A Task",
-                 category="No Category",
-                 description="",
-                 priority="medium",
-                 due_date:list[int]=None,
+                 parent = None,
+                 name: str = "A Task",
+                 category = "No Category",
+                 description = "",
+                 priority = "medium",
+                 start_date: date = date.today(),
+                 end_date: date = None,
                  assignee=None):
         '''
         :param parent: py:class:'Node', optional
@@ -40,26 +46,25 @@ class Task(Node):
         :param description: str, optional
         :param priority: str, optional
             Priority of the task: "low", "medium" and "high"
-        :param due_date: list[int], optional
-            The task's due date, as a list or tuple of (year, month, day)
+        :param start_date: :py:class:'datetime.date', optional
+            The task's start date. If not specified, the current date is used.
+        :param end_date: :py:class:'datetime.date', optional
+            The task's end date. If not specified, the current date is used.
         :param assignee: str, optional
             The name of the person who is assigned the task.
         '''
-        if due_date is None:
-            due_date = datetime.now()
-            self.due_date = (due_date.year,
-                        due_date.month,
-                        due_date.day)
-        else:
-            try:
-                assert len(due_date) == 3, f"Invalid due date {due_date}. Due date needs to be a list or a tuple of (year, month, day)."
-            except:
-                raise ValueError(f"Invalid due date {due_date}. Due date needs to be a list or a tuple of (year, month, day).")
-        self.name, self.description, self.assignee = \
-        name, description, assignee
-        self._priority = None
-        self.priority = priority
-        raise NotImplementedError("Change due date into start date.)
+        super().__init__(name=name,
+                         parent=parent) # A task is a node of a tree
+        # Get the argument names passed to the __init__ function
+        argvalues = getargvalues(currentframe())
+        attribute_names = argvalues.args
+        attribute_names.remove('parent')
+        for a in attribute_names:
+            # Initialize the internal attributes to 'None'
+            setattr(self, f"_{a}", None)
+        for a in attribute_names:
+            # Set the corresponding properties to the passed argument values
+            setattr(self, a, argvalues.locals[a])
 
     @property
     def start_date(self):
@@ -67,18 +72,38 @@ class Task(Node):
 
     @start_date.setter
     def start_date(self, value):
-        try:
-            assert len(value) == 3, f"Invalid start date {value}. Start date needs to be a list or a tuple of (year, month, day)."
-        except:
-            raise ValueError(
-                f"Invalid start date {value}. Start date needs to be a list or a tuple of (year, month, day)."
-        self._start_date = value\
+        if value is None:
+            self._start_date = date.today()
+        else:
+            if 'datetime.date' not in str(type(value)):
+                raise TypeError(f'Invalid start date type {type(value)}. Accepted types are ("datetime.date")')
+            if self.end_date is not None:
+                if value > self.end_date:
+                    assert ValueError('Start date ({value}) is greater than end date ({self.end_date})')
+            self._start_date = value
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, value):
+        if value is None:
+            self._end_date = date.today()
+        else:
+            if 'datetime.date' not in str(type(value)):
+                raise TypeError(f'Invalid end date type {type(value)}. Accepted types are ("datetime.date")')
+            if self.end_date is not None:
+                if value < self.start_date:
+                    assert ValueError('end date ({value}) is smaller than start date ({self.start_date})')
+            self._end_date = value
 
     @property
     def priority(self):
         return self._priority
+
     @priority.setter
     def priority(self, value):
-        assert value in PRIORITY_LEVELS, \
-            f"Invalid priority level {value}. Accepted values are {PRIORITY_LEVELS}"
+        if value not in PRIORITY_LEVELS:
+            raise ValueError(f"Invalid priority level {value}. Accepted values are {PRIORITY_LEVELS}")
         self._priority = value
